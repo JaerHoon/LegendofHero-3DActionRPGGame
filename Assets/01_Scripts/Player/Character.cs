37510029dd0e071daa0f1ca60f7273938b629c7b
yangstar98 <167849650+yangstar98@users.noreply.github.com>
2024-05-29 16:00:46 +0900

    플레이어 스킬

--- Assets/01_Scripts/Player/Character.cs
+++ Assets/01_Scripts/Player/Character.cs
@@ -7,9 +7,17 @@ public class Character : MonoBehaviour
     protected int playerHp;
     protected float playerAttackCD;
     protected float playerSkillCD;
+    protected float playerNonHitCD_cur;//현재 쿨타임
+    protected float playerNonHitCD;//현재 쿨타임
     protected float playerGCD;
     protected float playerSpeed;
-    
+    protected float playerCritDamage;
+
+    protected float playerAttackChargeRate;
+    protected float playerSkillChargeRate;
+    protected float playerNonHitTime;
+
+    protected bool isNonHit;
 
 
 
@@ -17,6 +25,7 @@ public class Character : MonoBehaviour
     protected Skills playerSkill;//스킬 정보
 
     protected SkillParent[] skills = new SkillParent[2];
+
 
 
 
@@ -52,7 +61,7 @@ public class Character : MonoBehaviour
         {
             yield return new WaitForSeconds(0.1f);
             playerGCD -= 0.1f;
-            print($"남은 GCD : {playerGCD}초");
+            //print($"남은 GCD : {playerGCD}초");
             if (playerGCD < 0)
             {
                 playerGCD = 0;
@@ -95,24 +104,61 @@ public class Character : MonoBehaviour
         }
     }
 
+    float CalTotalDamage(float Damage)//아이템 효과를 적용, 크리티컬 적용, 최종 데미지 데미지
+    {
+        int randCirtRate = Random.Range(0, 10);
+        if (randCirtRate < 2) playerCritDamage = 1.5f + ItemManager.instance.itemToAddCritDamage;
+        else playerCritDamage = 1f;
+        return (Damage + ItemManager.instance.itemToAllSkillDamage)  * playerCritDamage;//(스킬 데미지 + 추가 위력) *  크리티컬
+    }
+
+    IEnumerator CoolDownNonHitTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            playerNonHitCD_cur -= 0.1f;
+            if (playerNonHitCD_cur < 0)
+            {
+                playerNonHitCD_cur = 0;
+                isNonHit = false;
+                print("방어스킬 준비 완료!");
+                yield break;
+            }
+
+        }
+    }
+
     protected void OnUseSkill()
     {
-        if (playerGCD > 0) return;
-        if (Input.GetMouseButtonDown(0) && playerAttackCD == 0)
+       
+        if (!isNonHit)//무적 시간에는 스킬 사용 불가
         {
-            skills[0].UsedSkill(playerAttack.Damage, playerAttack.CD, playerAttack.GCD, playerAttack.Scale, playerAttack.SpeedRate, playerAttack.EventRate, playerAttack.DamageCount, playerAttack.SkillCount, playerAttack.DamageRate);
-            playerAttackCD = playerAttack.CD;
-            playerGCD = playerAttack.GCD;
-            GlobalCoolDownSKill();
-            CoolDownAttack();
-        }
-        if (Input.GetMouseButtonDown(1) && playerSkillCD == 0)
-        {
-            skills[1].UsedSkill(playerSkill.Damage, playerSkill.CD, playerSkill.GCD, playerSkill.Scale, playerSkill.SpeedRate, playerSkill.EventRate, playerSkill.DamageCount, playerSkill.SkillCount, playerSkill.DamageRate);
-            playerSkillCD = playerSkill.CD;
-            playerGCD = playerSkill.GCD;
-            GlobalCoolDownSKill();
-            CoolDownSKill();
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                isNonHit = true; print("1.5초간 무적!");
+                playerNonHitCD_cur = playerNonHitCD;
+                StartCoroutine(CoolDownNonHitTime());
+            }
+
+            if (playerGCD > 0) return;
+
+            if (Input.GetMouseButtonDown(0) && playerAttackCD == 0)
+            {
+                skills[0].UsedSkill(CalTotalDamage(playerAttack.Damage) * ItemManager.instance.itemToAttackDamageRate * playerAttackChargeRate, playerAttack.CD, playerAttack.GCD, playerAttack.Scale, playerAttack.SpeedRate, playerAttack.EventRate, playerAttack.DamageCount, playerAttack.SkillCount, playerAttack.DamageRate);
+                playerAttackCD = playerAttack.CD;
+                playerGCD = playerAttack.GCD + ItemManager.instance.itemToSkillGCD;
+                GlobalCoolDownSKill();
+                CoolDownAttack();
+            }
+            if (Input.GetMouseButtonDown(1) && playerSkillCD == 0)
+            {
+                skills[1].UsedSkill(CalTotalDamage(playerSkill.Damage) * playerSkillChargeRate, playerSkill.CD, playerSkill.GCD, playerSkill.Scale, playerSkill.SpeedRate, playerSkill.EventRate, playerSkill.DamageCount, playerSkill.SkillCount, playerSkill.DamageRate);
+                playerSkillCD = playerSkill.CD;
+                playerGCD = playerSkill.GCD + ItemManager.instance.itemToSkillGCD;
+                GlobalCoolDownSKill();
+                CoolDownSKill();
+            }
         }
     }
 }
--- Assets/01_Scripts/Player/Warrior.cs
+++ Assets/01_Scripts/Player/Warrior.cs
@@ -7,6 +7,7 @@ public class Warrior : Character
     // Start is called before the first frame update
     void Start()
     {
+        WarriorInit();
         playerAttack = SkillManager.instance.WarriorSkills[0];
         playerSkill = SkillManager.instance.WarriorSkills[7];
         skills[0] = this.gameObject.GetComponent<WarriorAttack0>();
@@ -18,8 +19,16 @@ public class Warrior : Character
         playerHp = 10;
         playerAttackCD = 0;
         playerSkillCD = 0;
+        playerNonHitCD_cur = 0;
+        playerNonHitCD = 15;
         playerGCD = 0;
         playerSpeed = 1;
+        playerCritDamage = 1.5f;
+        playerAttackChargeRate = 1;
+        playerSkillChargeRate = 1;
+        playerNonHitTime = 1.5f;
+
+        isNonHit = false;
     }
 
     void OnChangeSkills(int SKillNum)
@@ -35,7 +44,6 @@ public class Warrior : Character
     }
 
 
-    
 
     // Update is called once per frame
     void Update()
