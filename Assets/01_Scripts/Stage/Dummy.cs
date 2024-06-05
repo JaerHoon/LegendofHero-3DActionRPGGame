62d67bcca3574c2987082a88e7f53a836ab68b72
KimJaeHoon <kmandm@naver.com>
2024-06-05 18:18:38 +0900

    asdsad

---
+++ Assets/01_Scripts/Stage/Dummy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnHit(float damage)
+    {
+        GameObject pool = PoolFactroy.instance.GetPool(Consts.DamageText);
+        DamageText damageText = pool.GetComponent<DamageText>();
+        damageText.OnTexting(damage);
+        pool.transform.position =
+            new Vector3(transform.position.x, transform.position.y + 2.5f, transform.position.z);
+
+    }
+}
