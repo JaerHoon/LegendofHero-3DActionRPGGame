30823edbc9895de2ca1b03b272766fbd5ae7e6d3
KimJaeHoon <kmandm@naver.com>
2024-06-17 17:19:00 +0900

    asd

--- Assets/01_Scripts/Manager/CharacterManager.cs
+++ Assets/01_Scripts/Manager/CharacterManager.cs
@@ -5,7 +5,23 @@ using UnityEngine;
 public class CharacterManager : MonoBehaviour
 {
     public static CharacterManager instance;
-   
+
+    public static  CharacterManager Instance
+    {
+        get
+        {
+            // 인스턴스가 없는 경우에 접근하려 하면 인스턴스를 할당해준다.
+            if (!instance)
+            {
+                instance = FindObjectOfType(typeof(CharacterManager)) as CharacterManager;
+
+                if (instance == null)
+                    Debug.Log("no Singleton obj");
+            }
+            return instance;
+        }
+    }
+
     public SkillData KnightData;
    
     public SkillData ArcherData;
@@ -17,6 +33,16 @@ public class CharacterManager : MonoBehaviour
 
     private void Awake()
     {
-        if (instance == null) instance = this;
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        // 인스턴스가 존재하는 경우 새로생기는 인스턴스를 삭제한다.
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+        // 아래의 함수를 사용하여 씬이 전환되더라도 선언되었던 인스턴스가 파괴되지 않는다.
+        DontDestroyOnLoad(gameObject);
     }
 }
