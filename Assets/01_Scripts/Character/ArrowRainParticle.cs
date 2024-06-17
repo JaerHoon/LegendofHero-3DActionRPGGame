52e4ae184bd2925dc109e86a64c7e0b2c639fbf2
GHG0832 <sky08321@gmail.com>
2024-06-17 17:16:50 +0900

    0617 궁수 이미지 저장

--- Assets/01_Scripts/Character/ArrowRainParticle.cs
+++ Assets/01_Scripts/Character/ArrowRainParticle.cs
@@ -12,18 +12,17 @@ public class ArrowRainParticle : MonoBehaviour
 
     public float DestroyDuration = 4.0f;
     public float DestroyLifetime = 2.0f;
-<<<<<<< HEAD
+
     
-=======
+
     private void Awake()
     {
-        if (instance == null)
-            instance = this;
+
         Aoe = GetComponent<ParticleSystem>();
         circle = transform.GetChild(5).GetComponent<ParticleSystem>();
         Light = transform.GetChild(6).GetComponent<ParticleSystem>();
     }
->>>>>>> 8bc1200766ea6e3f33677732cd38e736c414ce88
+
     void Start()
     {
         
